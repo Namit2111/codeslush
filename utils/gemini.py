69eb29cc399c99be1b3ac5c24ea062836d6e066a
Nmait2111 <namitjain2111@gmail.com>
2024-07-22 12:35:12 +0530

    secret key

--- utils/gemini.py
+++ utils/gemini.py
@@ -3,10 +3,11 @@ import os
 
 def chat(prompt):
 
-    genai.configure(api_key="AIzaSyCazJEmpYATyI34DZUKsHfwIoFq94yGALI")
+    genai.configure(api_key=os.getenv('GENAI_API_KEY'))
 
     # Using `response_mime_type` requires one of the Gemini 1.5 Pro or 1.5 Flash models
     model = genai.GenerativeModel('gemini-1.5-flash')
 
     response = model.generate_content(prompt)
     return response.text
+
